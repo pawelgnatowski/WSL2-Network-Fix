10f69c2738231b14dff820f679630e87cd3c226c
matt wilkie <maphew@gmail.com>
2021-10-05 00:06:26 -0700

    some error catching, incomplete
    we successfully abort after 10 attempts to get the vmswitch. I don't
    know why attempt to catch permissions failure at 1st attempt isn't
    working though.

--- resetWindowsNet.ps1
+++ resetWindowsNet.ps1
@@ -10,14 +10,14 @@ function ConfigureWSLNetwork {
      
     Write-Output "Starting WSL..." >> $logPath
     
-    $wslStatus = Get-Process -Name "wsl" -ErrorAction SilentlyContinue
+    $wslStatus = Get-Process -Name "wsl" -ErrorAction Continue
     if (!($wslStatus)) {
         Start-Job -ScriptBlock { Start-Process -FilePath "wsl.exe" -WindowStyle hidden }
     }   
     
     Do {
 
-        $wslStatus = Get-Process -Name "wsl" -ErrorAction SilentlyContinue
+        $wslStatus = Get-Process -Name "wsl" -ErrorAction Continue
     
         If (!($wslStatus)) { Write-Output 'Waiting for WSL2 process to start' >> $logPath ; Start-Sleep 1 }
         
@@ -46,15 +46,16 @@ wsl exit
 
 wsl -l -v *>> $logPath
 
-
 $started = $false
+$err = @() 
 
 Do {
-
-    $status = Get-VMSwitch WSL -ErrorAction SilentlyContinue
+    $status = Get-VMSwitch WSL -ErrorAction SilentlyContinue -ErrorVariable +err
     Write-Output $status >> $logPath
+    If ($err[0] -match 'do not have required permission') { Write-Output $err >> $logPath; throw $err }
+    If ($err.count -eq 10) {Write-Output '*** Error No WSL VM switch after 10 attempts' >> $logPath; throw $err}
+
     If (!($status)) { Write-Output 'Waiting for WSL swtich to get registered' ; Start-Sleep 1 }
-    
     Else {
         Write-Output  "WSL Network found" ; 
         $started = $true; 
